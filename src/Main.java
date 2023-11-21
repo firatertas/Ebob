4cf7d05d3aa8ba94c790c0300cb5f6fb30e8fcf3
Fırat <125742256+firatertas@users.noreply.github.com>
2023-11-21 15:07:51 +0300

    Ebob ve ekok perojes yapıldı

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, ebob = 1, ekok = 1, search;
+        System.out.print("n1 değeri girin : ");
+        n1 = sc.nextInt();
+        System.out.print("n2 değeri girin : ");
+        n2 = sc.nextInt();
+
+        if (n1 < n2) {
+            int i = 1;
+            while (i <= n1) {
+                if (n1 % i == 0 && n2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+                ekok = (n1 * n2) / ebob;
+            }
+        } else {
+            int i = 1;
+            while (i <= n2) {
+                if (n1 % i == 0 && n2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+                ekok = (n1 * n2) / ebob;
+            }
+        }
+        do {
+            System.out.println("1- Ebob\n2- Ekok\n3- Ebob ve Ekok\n4- İşlemi sonlandır\nLütfen seçmekistediğiniz işlemi tıklayınız : ");
+            search = sc.nextInt();
+
+            switch (search) {
+                case 1:
+                    System.out.println(n1 + " ve " + n2 + " Değerlerinin Ebob'u : " + ebob);
+                    break;
+                case 2:
+                    System.out.println(n1 + " ve " + n2 + " değerinin Ekok'u : " + ekok);
+                    break;
+                case 3:
+                    System.out.println(n1 + " ve " + n2 + "\nEbob : " + ebob + "\nEkok : " + ekok);
+                    break;
+                case 4:
+            }
+        }while (search!=4);
+            System.out.println("İşleminiz Sonlanmıştır!");
+    }
+}
